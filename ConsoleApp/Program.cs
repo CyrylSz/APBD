2fc4ca05705a7a1d534601ce22f27b605ebb15eb
Golomp <s30624@pjwstk.edu.pl>
2025-03-12 08:35:59 +0100

    Dodano metodę zwracającą maksymalną wartość z tablicy

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         int[] numbers = { 5, 10, 15, 20 };
         Console.WriteLine("Średnia: " + ObliczSrednia(numbers));
+        Console.WriteLine("Maksymalna wartość: " + ZnajdzMaksimum(numbers));
     }
 
     public static double ObliczSrednia(int[] numbers)
@@ -19,5 +20,20 @@ class Program
             suma += number;
         }
         return (double)suma / numbers.Length;
+    }
+    public static int ZnajdzMaksimum(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta.");
+
+        int max = numbers[0];
+        foreach (var number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
     }
 }

d0b3be208d8d771f1ab3c3d07f171a233fa59438
Golomp <s30624@pjwstk.edu.pl>
2025-03-12 08:30:25 +0100

    Dodano metodę obliczającą średnią

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Modyfikacja 3!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, 10, 15, 20 };
+        Console.WriteLine("Średnia: " + ObliczSrednia(numbers));
+    }
+
+    public static double ObliczSrednia(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta.");
+
+        int suma = 0;
+        foreach (var number in numbers)
+        {
+            suma += number;
+        }
+        return (double)suma / numbers.Length;
+    }
+}
